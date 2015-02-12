4102c627b305b71a6ff18c8733f344422c5b1c9a
Shubhanshu Gupta <shubhanshu2007@gmail.com>
2015-02-12 14:33:54 +0530

    Question vocabulary added in RDF vocabularies

---
+++ lib/rdf_vocabularies/questions_vocab.rb
@@ -0,0 +1,6 @@
+class QuestionsVocab < RDF::Vocabulary("http://example.com/ontologies/QuestionsAndAnswers/0.1/") 
+  property :Question
+  property :Answer  
+  property :askedQuestion
+  property :hasAnswer
+end

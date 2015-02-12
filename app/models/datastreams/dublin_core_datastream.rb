36654de299a713857183e753c74b9dd96db81502
Shubhanshu Gupta <shubhanshu2007@gmail.com>
2015-02-12 13:55:21 +0530

    Created an RDF datastream

---
+++ app/models/datastreams/dublin_core_datastream.rb
@@ -0,0 +1,9 @@
+require 'active-fedora'
+class DublinCoreDatastream < ActiveFedora::RDFDatastream
+  property :title, predicate: RDF::DC.title
+  property :creator, predicate: RDF::DC.creator
+  property :contributor, predicate: RDF::DC.contributor
+  property :date, predicate: RDF::DC.date
+  property :subject, predicate: RDF::DC.subject
+  property :relation, predicate: RDF::DC.relation
+end

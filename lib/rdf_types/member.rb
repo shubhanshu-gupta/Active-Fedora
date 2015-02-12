16878cfb494e176daf5c280c892d80dedbd2c480
Shubhanshu Gupta <shubhanshu2007@gmail.com>
2015-02-12 14:23:10 +0530

    Member RDF type defined

---
+++ lib/rdf_types/member.rb
@@ -0,0 +1,9 @@
+require "active-fedora"
+require  "./lib/rdf_vocabularies/questions_vocab.rb"
+class Member < ActiveFedora::Rdf::Resource
+  configure type: RDF::FOAF.Person
+  property :nick, predicate: RDF::FOAF.nick
+  property :givenName, predicate: RDF::FOAF.givenName
+  property :familyName, predicate: RDF::FOAF.familyName
+  property :questions, predicate: QuestionsVocab.askedQuestion, class_name: "Question"
+end
